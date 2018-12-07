381d1a7596e9acc0286c6bcc6de4b45a59ae539e
Miguel Montero <mguemontero@gmail.com>
2018-12-07 13:16:48 +0000

    Created some objects

--- Milkyway.py
+++ Milkyway.py
@@ -5,15 +5,16 @@ class Galaxy(object):
 	"""A galaxy is a gravitationally bound system of stars, stellar remnants, interstellar gas, 
 	   dust, and dark matter.
 	"""
-	def __init__(self, arg):
-		pass
+	def __init__(self, name, shape):
+		self.name = name
+		self.shape = shape
 		
 class Planet(object):
 	"""A planet is an astronomical body orbiting a star or stellar remnant that is massive enough 
 	   to be rounded by its own gravity, is not massive enough to cause thermonuclear fusion, and 
 	   has cleared its neighbouring region of planetesimals.
 	"""
-	def __init__(self, name, orbit, axial_tilt, rotation, color):
+	def __init__(self, name, orbit=None, axial_tilt=None, rotation=None, color=None):
 		self.name = name
 		self.orbit = orbit
 		self.axial_tilt = axial_tilt
@@ -32,4 +33,11 @@ class Star(object):
 		self.temperature = temperature
 
 Planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus','Neptune']
+solar_system = []
+
+for planeta in Planets:
+	solar_system.append(Planet(planeta))
+# [Planet(planeta) for planeta in Planets]
+
 sun = Star('Sun')
+Milky_way = Galaxy('Milky Way', 'spiral')
