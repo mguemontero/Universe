f26177daeb35f208a793486fa3282961d1a98e84
Miguel Montero <mguemontero@gmail.com>
2018-12-07 12:33:48 +0000

    Created module with main objects

---
+++ Milkyway.py
@@ -0,0 +1,24 @@
+"""
+	Defining classes for modeling Solar System.
+"""
+class Galaxy(object):
+	"""A galaxy is a gravitationally bound system of stars, stellar remnants, interstellar gas, 
+	   dust, and dark matter.
+	"""
+	def __init__(self, arg):
+		pass
+		
+class Planet(object):
+	"""A planet is an astronomical body orbiting a star or stellar remnant that is massive enough 
+	   to be rounded by its own gravity, is not massive enough to cause thermonuclear fusion, and 
+	   has cleared its neighbouring region of planetesimals.
+	"""
+	def __init__(self, arg):
+		pass
+
+class Star(object):
+	"""A star is type of astronomical object consisting of a luminous spheroid of plasma
+	   held together by its own gravity.
+	"""
+	def __init__(self, arg):
+		pass
