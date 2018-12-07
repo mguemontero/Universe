db137d39a064437efa963610b456c194f834085d
Miguel Montero <mguemontero@gmail.com>
2018-12-07 12:45:34 +0000

    Added __init__ method

--- Milkyway.py
+++ Milkyway.py
@@ -13,12 +13,23 @@ class Planet(object):
 	   to be rounded by its own gravity, is not massive enough to cause thermonuclear fusion, and 
 	   has cleared its neighbouring region of planetesimals.
 	"""
-	def __init__(self, arg):
-		pass
+	def __init__(self, name, orbit, axial_tilt, rotation, color):
+		self.name = name
+		self.orbit = orbit
+		self.axial_tilt = axial_tilt
+		self.rotation = rotation
+		self.color = color
+
 
 class Star(object):
 	"""A star is type of astronomical object consisting of a luminous spheroid of plasma
 	   held together by its own gravity.
 	"""
-	def __init__(self, arg):
-		pass
+	def __init__(self, name, age=None, mass=None, temperature=None):
+		self.name = name
+		self.age = age
+		self.mass = mass
+		self.temperature = temperature
+
+Planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus','Neptune']
+sun = Star('Sun')
