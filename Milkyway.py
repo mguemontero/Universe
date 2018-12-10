3abf0c787ef5827d95530aa67b1fc8572efa7968
Miguel Montero <mguemontero@gmail.com>
2018-12-10 17:21:14 +0000

    Heritage from object removed

--- Milkyway.py
+++ Milkyway.py
@@ -1,7 +1,7 @@
 """
 	Defining classes for modeling Solar System.
 """
-class Galaxy(object):
+class Galaxy:
 	"""A galaxy is a gravitationally bound system of stars, stellar remnants, interstellar gas, 
 	   dust, and dark matter.
 	"""
@@ -9,7 +9,7 @@ class Galaxy(object):
 		self.name = name
 		self.shape = shape
 		
-class Planet(object):
+class Planet:
 	"""A planet is an astronomical body orbiting a star or stellar remnant that is massive enough 
 	   to be rounded by its own gravity, is not massive enough to cause thermonuclear fusion, and 
 	   has cleared its neighbouring region of planetesimals.
@@ -22,7 +22,7 @@ class Planet(object):
 		self.color = color
 
 
-class Star(object):
+class Star:
 	"""A star is type of astronomical object consisting of a luminous spheroid of plasma
 	   held together by its own gravity.
 	"""
@@ -38,6 +38,17 @@ solar_system = []
 for planeta in Planets:
 	solar_system.append(Planet(planeta))
 # [Planet(planeta) for planeta in Planets]
+
+# Planets as dictionaries
+
+Earth = { 'name': 'Earth',  'color': 'blue', 'orbit' = None }
+Mars = { 'name': 'Mars',  'color': 'red', 'orbit' = None }
+
+# Planets = [Earth, Mars, ...]
+
+Earth['orbit']
+
+# TODO: Planets as namedtuple
 
 sun = Star('Sun')
 Milky_way = Galaxy('Milky Way', 'spiral')
